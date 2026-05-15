fa88bbbed3bbb9eb7b6b7bcc2d8e8bff63e691c1
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-05-15 02:00:23 +0200

    feat: add routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {MessageComponent} from './message/view/message/message.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'message',
+    pathMatch: 'full',
+  },
+  {
+    path: "message",
+    component: MessageComponent
+  }
+];
